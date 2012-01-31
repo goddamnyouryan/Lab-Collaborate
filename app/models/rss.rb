031dd84f8789f4a5289227d261ce36be1ba2ada8
Ryan MacInnes <ryan.macinnes@gmail.com>
2012-01-30 19:32:37 -0800

    Added validation on feeds

--- app/models/rss.rb
+++ app/models/rss.rb
@@ -1,2 +1,8 @@
 class Rss < ActiveRecord::Base
+  
+  validate :check_rss
+  
+  def check_rss
+    errors.add(:base, "Not a Valid RSS Feed.") if Feedzirra::Feed.fetch_and_parse(feed) == 0
+  end  
 end
